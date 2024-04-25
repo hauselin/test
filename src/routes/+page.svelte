9673c2300cf79fbba27f100c82147061195d39d9
Hause Lin <hauselin@gmail.com>
2024-04-25 00:02:20 -0400

    Update code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,65 @@
 <script>
-	import { useChat } from 'ai/svelte'
+	import { useChat } from "ai/svelte";
+	import { onMount, onDestroy } from "svelte";
 
-	const { input, handleSubmit, messages } = useChat()
+	const { input, handleSubmit, messages } = useChat();
+
+	let streamEnded = false;
+	let timeoutId;
+
+	let n_messages = 0;
+	const TIMEOUT_DURATION = 2500;
+
+	$: if ($messages) {
+		// console.log($messages);
+		if ($messages.length > n_messages) {
+			n_messages = $messages.length;
+			clearTimeout(timeoutId);
+			timeoutId = setTimeout(() => {
+				streamEnded = true;
+				console.log("Streaming has ended");
+			}, TIMEOUT_DURATION);
+		}
+	}
+
+	onMount(() => {
+		timeoutId = setTimeout(() => {
+			streamEnded = true;
+		}, TIMEOUT_DURATION);
+	});
+
+	onDestroy(() => {
+		clearTimeout(timeoutId);
+	});
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Chatbot</title>
+	<meta name="description" content="Svelte chatbot" />
 </svelte:head>
 
 <section>
-	<h1>useChat</h1>
-	<ul>
+	<h2>messages count: {n_messages}</h2>
+
+	<div>
 		{#each $messages as message}
-			<li>{message.role}: {message.content}</li>
+			<p>
+				{#if message.role === "user"}
+					You:
+				{:else if message.role === "assistant"}
+					Bot:
+				{/if}
+				{message.content}
+			</p>
 		{/each}
-	</ul>
-	<form on:submit={handleSubmit}>
-		<input bind:value={$input} />
-		<button type="submit">Send</button>
-	</form>
+	</div>
+
+	<div>
+		<form on:submit={handleSubmit}>
+			<input bind:value={$input} />
+			<button type="submit">Submit</button>
+		</form>
+	</div>
 </section>
 
 <style>
@@ -28,10 +68,6 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
+		flex: 0;
 	}
 </style>
